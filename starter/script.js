8aa8f691b84dfc68cbc115c012023247bf3272e0
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2024-01-11 16:42:42 +0530

    ....

--- starter/script.js
+++ starter/script.js
@@ -63,8 +63,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const displayMovements = function (movements) {
   movements.forEach(function (mov, index) {
-    containerMovements.innerHTML = '';
-    
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -81,6 +79,53 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data
+ into an array (one array for each).
+ For now, they are just interested in knowing whether a dog is an adult or a puppy. 
+ A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'),
+ and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs!
+ So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice 
+ to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") 
+or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+
+//soln
+
+function checkDogs(dogJulia, dogsKate) {
+  let newArrJulia = dogJulia.slice();
+  newArrJulia.splice(0, 1);
+  newArrJulia.splice(-2);
+  console.log(newArrJulia);
+
+  const bothArr = [...newArrJulia, ...dogsKate];
+
+  bothArr.forEach(function (dog, i) {
+    dog >= 3
+      ? console.log(`dog number ${i + 1} is adult and its ${dog} years old`)
+      : console.log(`dog number ${i + 1} is still puppy 🐶`);
+  });
+}
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+*/
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -208,3 +253,38 @@ currenciesUnique.forEach(function (value, valueAlso, set) {
   console.log(`${valueAlso}:${value}`);
 });
 */
+
+///+++++++++++++++DATA TRANSFORMERS+++++++++++++++++++
+//MAP,FILTER,REDUCE
+
+//++++++++++++++MAP+++++++++++++++
+const eurToUsd = 1.1;
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movementsUSD = movements.map(function (mov) {
+  return eurToUsd * mov;
+});
+console.log(movements);
+console.log(movementsUSD);
+
+let movementsUSDfor = [];
+for (const mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd);
+}
+
+console.log(movementsUSDfor);
+
+//
+const movementsUSDUseArrow = movements.map(mov => mov * eurToUsd);
+console.log(movementsUSDUseArrow);
+
+const movementsDescription = movements.map((mov, i) => {
+  `Movement ${i + 1}: You ${mov > 1 ? 'Deposited' : 'Withdrawn'}`;
+  if (mov > 0) {
+    return `Movements ${i + 1}:You deposited ${mov}`;
+  } else {
+    return `Movements ${i + 1}:You withdraw ${Math.abs(mov)}`;
+  }
+});
+console.log(movementsDescription);
+
+
